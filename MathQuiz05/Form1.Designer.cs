d744477592003322a05e21aeac31155e9108a54d
Asher Abrams <asher813@aol.com>
2019-02-28 06:19:28 -0800

    Step 2:  Create random addition problem.

--- MathQuiz05/Form1.Designer.cs
+++ MathQuiz05/Form1.Designer.cs
@@ -320,6 +320,7 @@
             this.startButton.TabIndex = 1;
             this.startButton.Text = "START QUIZ";
             this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // Form1
             // 
