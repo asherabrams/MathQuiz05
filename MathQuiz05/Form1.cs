604232634b3347ef277b6f5176e6040eab2c691f
Asher Abrams <asher813@aol.com>
2019-03-05 05:52:05 -0800

    Step 6:  Add subtraction problem.

--- MathQuiz05/Form1.cs
+++ MathQuiz05/Form1.cs
@@ -15,7 +15,8 @@ namespace MathQuiz05
         Random randomizer = new Random();
         int addend1;
         int addend2;
-
+        int minuend;
+        int subtrahend;
 
         int timeLeft;
 
@@ -23,11 +24,15 @@ namespace MathQuiz05
         {
             addend1 = randomizer.Next(51);
             addend2 = randomizer.Next(51);
-
             plusLeftLabel.Text = addend1.ToString();
             plusRightLabel.Text = addend2.ToString();
-
             sum.Value = 0;
+
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
 
             timeLeft = 30;
             timeLabel.Text = "30 SECONDS";
@@ -66,13 +71,15 @@ namespace MathQuiz05
                 timeLabel.Text = "TIME!";
                 MessageBox.Show("OUT OF TIME");
                 sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
                 startButton.Enabled = true;
             }
         }
 
         private bool CheckTheAnswer()
         {
-            if (addend1 + addend2 == sum.Value)
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value))
                 return true;
             else
                 return false;
